b47cf49dad75a6c2ef78d1eb409335f452a7b4fe
zhelev89 <zhelev89@yahoo.com>
2022-04-15 16:26:00 +0300

    now i created new project, made 2 entities(Person and car) and made connecting between Person - Cars (ManytoMany)

---
+++ exersiceBusinessLayerAndEndPoints/src/main/java/com/example/exersiceBusinessLayerAndEndPoints/entities/Car.java
@@ -0,0 +1,30 @@
+package com.example.exersiceBusinessLayerAndEndPoints.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tables")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @Column(name = "model", nullable = false)
+    private String model;
+
+    @Column(name = "year", nullable = false)
+    private int year;
+}
---
+++ exersiceBusinessLayerAndEndPoints/src/main/java/com/example/exersiceBusinessLayerAndEndPoints/entities/Person.java
@@ -0,0 +1,33 @@
+package com.example.exersiceBusinessLayerAndEndPoints.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "ucn", nullable = false, unique = true)
+    private String ucn;
+
+    @ManyToOne
+    private Car car;
+}
