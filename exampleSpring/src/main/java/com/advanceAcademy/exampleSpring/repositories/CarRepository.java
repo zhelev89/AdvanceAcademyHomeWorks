fd30c98d657ae2456c7d0dea8b2e421ae63980f0
zhelev89 <zhelev89@yahoo.com>
2022-04-16 11:16:42 +0300

    Just delete my old homework

--- exampleSpring/src/main/java/com/advanceAcademy/exampleSpring/repositories/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.advanceAcademy.exampleSpring.repositories;
-
-import com.advanceAcademy.exampleSpring.models.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-
-}
--- exampleSpring/src/main/java/com/advanceAcademy/exampleSpring/repositories/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.advanceAcademy.exampleSpring.repositories;
-
-import com.advanceAcademy.exampleSpring.models.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
